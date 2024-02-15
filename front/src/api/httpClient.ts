1baeb5c12c1ad7604df98afac1a182414f19255e
IvanShashkov <131527822+IvanShashkov@users.noreply.github.com>
2024-02-15 23:44:16 +0400

    Update httpClient.ts

--- front/src/api/httpClient.ts
+++ front/src/api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const config = {
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://task4-backend-pied.vercel.app/,
     crossDomain: true,
     headers: {
         'Content-Type': 'application/json',
